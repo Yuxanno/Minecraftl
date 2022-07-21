8bee7c98d428c6eefcf6fea499b5cb9832f708f5
1234567890 <ravshanovyuxanno0511@gmail.com>
2022-07-21 11:35:50 +0500

    50процент сделанно

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function myfunction(){
 function disNone(){
     document.getElementById("hod").style.display = "none";
 }
+
+
+function myfunction1(){
+    document.getElementById("infodowland").style.display = "block";
+}
+function disNone1(){
+    document.getElementById("infodowland").style.display = "none";
+}
